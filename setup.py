be07b4193b40d205c2d6519f9ba8214161e1c430
codophobia <shivamm389@gmail.com>
2016-12-23 01:53:03 +0530

    changed init.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'pycricbuzz',
+  packages = ['pycricbuzz'], 
+  version = '0.1',
+  description = 'A library for fetching live cricket scores from cricbuzz',
+  author = 'Shivam Mitra',
+  author_email = 'shivamm389@gmail.com',
+  license = 'GPLv2',
+  url = 'https://github.com/codophobia/pycricbuzz', 
+  download_url = 'https://github.com/codophobia/pycricbuzz/tarball/0.1', 
+  keywords = ['cricket', 'cricbuzz'], 
+  install_requires=[
+          'requests',
+          'beautifulsoup4',
+      ],
+  classifiers = [],
+)
