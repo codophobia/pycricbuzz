5bf2b313aaf80716f8d504b1046946e5610399a5
shivam.m <shivam.m@media.net>
2019-02-04 05:06:34 +0530

    Change package version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'pycricbuzz',
   packages = ['pycricbuzz'], 
-  version = '3',
+  version = '2.1',
   description = 'A library for fetching live cricket scores from cricbuzz',
   author = 'Shivam Mitra',
   author_email = 'shivamm389@gmail.com',
   license = 'GPLv2',
   url = 'https://github.com/codophobia/pycricbuzz', 
-  download_url = 'https://github.com/codophobia/pycricbuzz/tarball/3', 
+  download_url = 'https://github.com/codophobia/pycricbuzz/tarball/2.1', 
   keywords = ['cricket', 'cricbuzz'], 
   install_requires=[
           'requests',
