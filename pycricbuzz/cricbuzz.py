ad29cedf88d782ac9596542096476403ac3b66b1
shivam.m <shivam.m@media.net>
2019-02-04 04:37:51 +0530

    Change cricbuzz api path

--- pycricbuzz/cricbuzz.py
+++ pycricbuzz/cricbuzz.py
@@ -1,149 +1,201 @@
 import requests
-import json
-import sys
-from bs4 import BeautifulSoup
+import time
 
 class Cricbuzz():
-	url = "http://synd.cricbuzz.com/j2me/1.0/livematches.xml"
 	def __init__(self):
 		pass
 
-	def getxml(self,url):
+	def crawl_url(self,url):
 		try:
-			r = requests.get(url)
-		except requests.exceptions.RequestException as e: 
-			print (e)
-			sys.exit(1)
-		soup = BeautifulSoup(r.text,"html.parser")
-		return soup
+			r = requests.get(url).json()
+			return r
+		except Exception: 
+			raise
 
-	def matchinfo(self,match):
+	def players_mapping(self,mid):
+		url = "http://mapps.cricbuzz.com/cbzios/match/" + mid
+		match = self.crawl_url(url)
+		players = match.get('players')
 		d = {}
-		d['id'] = match['id']
-		d['srs'] = match['srs']
-		d['mchdesc'] = match['mchdesc']
-		d['mnum'] = match['mnum']
-		d['type'] = match['type']
-		d['mchstate'] = match.state['mchstate']
-		d['status'] = match.state['status']
+		for p in players:
+			d[int(p['id'])] = p['name']
+		t = {}
+		t[int(match.get('team1').get('id'))] = match.get('team1').get('name')
+		t[int(match.get('team2').get('id'))] = match.get('team2').get('name')
+		return d,t
+
+	def matchinfo(self,mid):
+		d = {}
+		d['id'] = mid
+		url = "http://mapps.cricbuzz.com/cbzios/match/" + mid
+		match = self.crawl_url(url)
+		
+		d['srs'] = match.get('series_name')
+		d['mnum'] = match.get('header',).get('match_desc')
+		d['type'] = match.get('header').get('type')
+		d['mchstate'] = match.get('header').get('state')
+		d['status'] = match.get('header').get('status')
+		d['venue_name'] = match.get('venue').get('name')
+		d['venue_location'] = match.get('venue').get('location')
+		d['toss'] = match.get('header').get('toss')
+		d['official'] = match.get('official')
+		d['start_time'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(match.get('header').get('start_time'))))
+
+
+		#squads
+		p_map,_ = self.players_mapping(mid)
+		team1 = {}
+		team1['name'] = match.get('team1').get('name')
+		t1_s = match.get('team1').get('squad')
+		if t1_s is None:
+			t1_s = []
+		team1['squad'] = [ p_map[id] for id in t1_s]
+		t1_s_b = match.get('team1').get('squad_bench')
+		if t1_s_b is None:
+			t1_s_b = []
+		team1['squad_bench'] =  [ p_map[id] for id in t1_s_b]
+		team2 = {}
+		team2['name'] = match.get('team2').get('name')
+		t2_s = match.get('team2').get('squad')
+		if t2_s is None:
+			t2_s = []
+		team2['squad'] = [ p_map[id] for id in t2_s]
+		t2_s_b = match.get('team2').get('squad_bench')
+		if t2_s_b is None:
+			t2_s_b = []
+		team2['squad_bench'] =  [ p_map[id] for id in t2_s_b]
+		d['team1'] = team1
+		d['team2'] = team2
 		return d
 
 	def matches(self):
-		xml = self.getxml(self.url)
-		matches = xml.find_all('match')
+		url = "http://mapps.cricbuzz.com/cbzios/match/livematches"
+		crawled_content = self.crawl_url(url)
+		matches = crawled_content['matches']
 		info = []
 		
 		for match in matches:
-			info.append(self.matchinfo(match))
+			info.append(self.matchinfo(match['match_id']))
 		return info
 
+	def find_match(self,id):
+		crawled_content = self.crawl_url(self.url)
+		matches = crawled_content['matches']
+
+		for match in matches:
+			if match['match_id'] == id:
+				return match
+		return None
+
 	def livescore(self,mid):
-		xml = self.getxml(self.url)
-                match = xml.find('match', {'id': mid})
 		data = {}
-		data['matchinfo'] = self.matchinfo(match)
-		tdata = data
 		try:
-			curl = match['datapath'] + "commentary.xml"
-			comm = self.getxml(curl)
-			mscr = comm.find('mscr')
-			batting = mscr.find('bttm')
-			bowling = mscr.find('blgtm')
-			batsman = mscr.find_all('btsmn')
-			bowler= mscr.find_all('blrs')
+			url = "http://mapps.cricbuzz.com/cbzios/match/" + mid + "/leanback.json"
+			comm = self.crawl_url(url)
+
+			if "over_summary" not in comm:
+				return data
+			batting = comm.get('bat_team')
+			bowling = comm.get('bow_team')
+			batsman = comm.get('batsman')
+			bowler = comm.get('bowler')
+
+			data["run_rate"] = comm.get("crr")
+			data["patnership"] = comm.get("prtshp")
+			data["prev_overs"] = comm.get("prev_overs")
+
+			if batsman is None:
+				batsman = []
+			if bowler is None:
+				bowler = []
 			d = {}
-			d['team'] = batting['sname']
+			d['team'] = batting.get('name')
 			d['score'] = []
 			d['batsman'] = []
 			for player in batsman:
-				d['batsman'].append({'name':player['sname'],'runs': player['r'],'balls':player['b'],'fours':player['frs'],'six':player['sxs']})
-			binngs = batting.find_all('inngs')
+				d['batsman'].append({'name':player['name'],'runs': player['r'],'balls':player['b'],'fours':player['4s'],'six':player['6s']})
+			binngs = batting.get('innings')
+			if binngs is None:
+				binngs = []
 			for inng in binngs:
-				d['score'].append({'desc':inng['desc'], 'runs': inng['r'],'wickets':inng['wkts'],'overs':inng['ovrs']})
+				d['score'].append({'inning_num':inng['id'], 'runs': inng['score'],'wickets':inng['wkts'],'overs':inng['overs'],'declare':inng.get('decl')})
 			data['batting'] = d
 			d = {}
-			d['team'] = bowling['sname']
+			d['team'] = bowling['name']
 			d['score'] = []
 			d['bowler'] = []
 			for player in bowler:
-				d['bowler'].append({'name':player['sname'],'overs':player['ovrs'],'maidens':player['mdns'],'runs':player['r'],'wickets':player['wkts']})
-			bwinngs = bowling.find_all('inngs')
+				d['bowler'].append({'name':player['name'],'overs':player['o'],'maidens':player['m'],'runs':player['r'],'wickets':player['w']})
+			bwinngs = bowling.get('innings')
+			if bwinngs is None:
+				bwinngs = []
 			for inng in bwinngs:
-				d['score'].append({'desc':inng['desc'], 'runs': inng['r'],'wickets':inng['wkts'],'overs':inng['ovrs']})
+				d['score'].append({'inning_num':inng['id'], 'runs': inng['score'],'wickets':inng['wkts'],'overs':inng['overs'],'declare':inng.get('decl')})
 			data['bowling'] = d
 			return data
-		except:
-			return tdata
+		except Exception:
+			raise
 
 	def commentary(self,mid):
-		xml = self.getxml(self.url)
-                match = xml.find('match', {'id': mid})
 		data = {}
-		data['matchinfo'] = self.matchinfo(match)
-		tdata = data
 		try:
-			curl = match['datapath'] + "commentary.xml"
-			comm = self.getxml(curl).find_all('c')
+			url = url = "http://mapps.cricbuzz.com/cbzios/match/" + mid + "/commentary"
+			comm = self.crawl_url(url).get('comm_lines')
 			d = []
 			for c in comm:
-				d.append(c.text)
+				d.append({"comm":c.get("comm"),"over":c.get("o_no")})
 			data['commentary'] = d
 			return data 
-		except:
-			return tdata
+		except Exception:
+			raise
 
 	def scorecard(self,mid):
-		xml = self.getxml(self.url)
-                match = xml.find('match', {'id': mid})
-		data = {}
-		data['matchinfo'] = self.matchinfo(match)
-		tdata = data
 		try:
-			surl = match['datapath'] + "scorecard.xml"
-			scard = self.getxml(surl)
-			scrs = scard.find('scrs')
-			innings = scrs.find_all('inngs')
-			squads = scard.find('squads')
-			teams = squads.find_all('team')
-			sq = []
-			sqd = {}
+			url = "http://mapps.cricbuzz.com/cbzios/match/" +  mid + "/scorecard.json"
+			scard = self.crawl_url(url)
+			p_map,t_map = self.players_mapping(mid)
 
-			for team in teams:
-				sqd['team'] = team['name']
-				sqd['members'] = []
-				members = team['mem'].split(", ")
-				for mem in members:
-					sqd['members'].append(mem)
-				sq.append(sqd.copy())
-			data['squad'] = sq	
+			innings = scard.get('Innings')
+			data = {}
 			d = []
 			card = {}
 			for inng in innings:
-				bat = inng.find('bttm')
-				card['batteam'] = bat['sname']
-				card['runs'] = inng['r']
-				card['wickets'] = inng['wkts']
-				card['overs'] = inng['ovrs']
-				card['runrate'] = bat['rr']
-				card['inngdesc'] = inng['desc']
-				batplayers = bat.find_all('plyr')
+				card['batteam'] = inng.get('bat_team_name')
+				card['runs'] = inng.get('score')
+				card['wickets'] = inng.get('wkts')
+				card['overs'] = inng.get('ovr')
+				card['inng_num'] = inng.get('innings_id')
+				extras = inng.get("extras")
+				card["extras"] = {"total":extras.get("t"),"byes":extras.get("b"),"lbyes":extras.get("lb"),"wides":extras.get("wd"),"nballs":extras.get("nb"),"penalty":extras.get("p")}
+				batplayers = inng.get('batsmen')
+				if batplayers is None:
+					batplayers = []
 				batsman = []
 				bowlers = []
+				fow = []
 				for player in batplayers:
-					status = player.find('status').text
-					batsman.append({'name':player['sname'],'runs': player['r'],'balls':player['b'],'fours':player['frs'],'six':player['six'],'dismissal':status})
+					status = player.get('out_desc')
+					p_name = p_map[int(player.get('id'))]
+					batsman.append({'name':p_name,'runs': player['r'],'balls':player['b'],'fours':player['4s'],'six':player['6s'],'dismissal':status})
 				card['batcard'] = batsman
-				bowl = inng.find('bltm')
-				card['bowlteam'] = bowl['sname']
-				bowlplayers = bowl.find_all('plyr')
+				card['bowlteam'] = t_map[int(inng.get("bowl_team_id"))]
+				bowlplayers = inng.get('bowlers')
+				if bowlplayers is None:
+					bowlplayers = []
 				for player in bowlplayers:
-					bowlers.append({'name':player['sname'],'overs':player['ovrs'],'maidens':player['mdns'],'runs':player['roff'],'wickets':player['wkts']})
+					p_name = p_map[int(player.get('id'))]
+					bowlers.append({'name':p_name,'overs':player['o'],'maidens':player['m'],'runs':player['r'],'wickets':player['w'],'wides':player['wd'],'nballs':player['n']})
 				card['bowlcard'] = bowlers
+				fall_wickets = inng.get("fow")
+				if fall_wickets is None:
+					fall_wickets = []
+				for p in fall_wickets:
+					p_name = p_map[int(p.get('id'))]
+					fow.append({"name":p_name,"wkt_num":p.get("wkt_nbr"),"score":p.get("score"),"overs":p.get("over")})
+				card["fall_wickets"] = fow
 				d.append(card.copy())
 			data['scorecard'] = d
 			return data
-		except:
-			return tdata
-		
-
+		except Exception:
+			raise
 
--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from setuptools import setup
 setup(
   name = 'pycricbuzz',
   packages = ['pycricbuzz'], 
-  version = '2',
+  version = '3',
   description = 'A library for fetching live cricket scores from cricbuzz',
   author = 'Shivam Mitra',
   author_email = 'shivamm389@gmail.com',
   license = 'GPLv2',
   url = 'https://github.com/codophobia/pycricbuzz', 
-  download_url = 'https://github.com/codophobia/pycricbuzz/tarball/0.6', 
+  download_url = 'https://github.com/codophobia/pycricbuzz/tarball/3', 
   keywords = ['cricket', 'cricbuzz'], 
   install_requires=[
           'requests',
-          'beautifulsoup4',
       ],
   classifiers = [],
 )
